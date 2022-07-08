91a236caff3f579b5d3e4e7a010785e7cc3a0bc6
Puja Sharma <pujas.works@gmail.com>
2022-07-08 21:10:13 +0530

    feat(jans-config-api): agama endpoint

--- jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/AgamaResource.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/rest/resource/auth/AgamaResource.java
@@ -6,14 +6,14 @@
 
 package io.jans.configapi.rest.resource.auth;
 
+import io.jans.agama.model.Flow;
+import static io.jans.as.model.util.Util.escapeLog;
 import com.github.fge.jsonpatch.JsonPatchException;
 import io.jans.configapi.core.rest.ProtectedApi;
-import io.jans.configapi.service.auth.AttributeService;
+import io.jans.configapi.service.auth.AgamaFlowService;
 import io.jans.configapi.util.ApiAccessConstants;
 import io.jans.configapi.util.ApiConstants;
-import io.jans.configapi.util.AttributeNames;
 import io.jans.configapi.core.util.Jackson;
-import io.jans.model.GluuAttribute;
 
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
@@ -27,116 +27,78 @@ import java.util.List;
 
 import org.slf4j.Logger;
 
-@Path(ApiConstants.JANS_AUTH + ApiConstants.CONFIG + ApiConstants.AGAMA)
+@Path(ApiConstants.CONFIG + ApiConstants.AGAMA)
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class AgamaResource extends ConfigBaseResource {
-
-    private static final String GLUU_ATTRIBUTE = "Agama Flow";
+    
+    private static final String AGAMA_FLOW = "Agama flow";
 
     @Inject
     Logger log;
 
     @Inject
-    AttributeService attributeService;
+    AgamaFlowService agamaFlowService;
 
     @GET
-    @ProtectedApi(scopes = { ApiAccessConstants.ATTRIBUTES_READ_ACCESS })
-    public Response getAttributes(@DefaultValue(DEFAULT_LIST_SIZE) @QueryParam(value = ApiConstants.LIMIT) int limit,
-            @DefaultValue("") @QueryParam(value = ApiConstants.PATTERN) String pattern,
-            @DefaultValue(ApiConstants.ALL) @QueryParam(value = ApiConstants.STATUS) String status) throws Exception {
-        List<GluuAttribute> attributes = new ArrayList<GluuAttribute>();
+    @ProtectedApi(scopes = { ApiAccessConstants.AGAMA_READ_ACCESS })
+    public Response getFlows(@DefaultValue("") @QueryParam(value = ApiConstants.PATTERN) String pattern,
+            @DefaultValue(DEFAULT_LIST_SIZE) @QueryParam(value = ApiConstants.LIMIT) int limit) throws Exception {
 
-        if (status.equalsIgnoreCase(ApiConstants.ALL)) {
-            if (!pattern.isEmpty() && pattern.length() >= 2) {
-                attributes = attributeService.searchAttributes(pattern, limit);
-            } else {
-                attributes = attributeService.searchAttributes(limit);
-            }
-        } else if (status.equalsIgnoreCase(ApiConstants.ACTIVE)) {
-            if (!pattern.isEmpty() && pattern.length() >= 2) {
-                attributes = attributeService.findAttributes(pattern, limit, true);
-            } else {
-                attributes = attributeService.searchAttributes(limit, true);
-            }
-
-        } else if (status.equalsIgnoreCase(ApiConstants.INACTIVE)) {
-            if (!pattern.isEmpty() && pattern.length() >= 2) {
-                attributes = attributeService.findAttributes(pattern, limit, false);
-            } else {
-                attributes = attributeService.searchAttributes(limit, false);
-            }
+        if (log.isDebugEnabled()) {
+            log.debug("Search Agama Flow with pattern:{}, sizeLimit:{}, ", escapeLog(pattern), escapeLog(limit));
         }
 
-        return Response.ok(attributes).build();
+        List<Flow> flows = new ArrayList<Flow>();
+        if (!pattern.isEmpty() && pattern.length() >= 2) {
+            flows = agamaFlowService.searchAgamaFlows(pattern, limit);
+        } else {
+            flows = agamaFlowService.getAllAgamaFlows(limit);
+        }
+
+        return Response.ok(flows).build();
     }
 
     @GET
-    @ProtectedApi(scopes = { ApiAccessConstants.ATTRIBUTES_READ_ACCESS })
-    @Path(ApiConstants.INUM_PATH)
-    public Response getAttributeByInum(@PathParam(ApiConstants.INUM) @NotNull String inum) {
-        GluuAttribute attribute = attributeService.getAttributeByInum(inum);
-        checkResourceNotNull(attribute, GLUU_ATTRIBUTE);
-        return Response.ok(attribute).build();
+    @ProtectedApi(scopes = { ApiAccessConstants.AGAMA_READ_ACCESS })
+    @Path(ApiConstants.QNAME_PATH)
+    public Response getFlowByName(@PathParam(ApiConstants.QNAME) @NotNull String flowName) {
+        if (log.isDebugEnabled()) {
+            log.debug("Search Agama with flowName:{}, ", escapeLog(flowName));
+        }
+        Flow flow = agamaFlowService.getFlowByName(flowName);
+        checkResourceNotNull(flow, AGAMA_FLOW);
+        return Response.ok(flow).build();
     }
 
     @POST
-    @ProtectedApi(scopes = { ApiAccessConstants.ATTRIBUTES_WRITE_ACCESS })
-    public Response createAttribute(@Valid GluuAttribute attribute) {
-        log.debug(" GluuAttribute details to add - attribute = " + attribute);
-        checkNotNull(attribute.getName(), AttributeNames.NAME);
-        checkNotNull(attribute.getDisplayName(), AttributeNames.DISPLAY_NAME);
-        checkResourceNotNull(attribute.getDataType(), AttributeNames.DATA_TYPE);
-        String inum = attributeService.generateInumForNewAttribute();
-        attribute.setInum(inum);
-        attribute.setDn(attributeService.getDnForAttribute(inum));
-        attributeService.addAttribute(attribute);
-        GluuAttribute result = attributeService.getAttributeByInum(inum);
-        return Response.status(Response.Status.CREATED).entity(result).build();
+    @ProtectedApi(scopes = { ApiAccessConstants.AGAMA_WRITE_ACCESS })
+    public Response createFlow(@NotNull Flow flow) {
+        log.debug(" Flow to be added flow:{} ",flow);
+     
+       //validate data
+        checkNotNull(flow.getQName(), Flow.ATTR_NAMES.QNAME);
+        checkNotNull(flow.getSource(),"SOURCE");
+        
+        agamaFlowService.addAgamaFlow(flow);
+        
+        flow = agamaFlowService.getFlowByName(flow.getQName());
+        return Response.status(Response.Status.CREATED).entity(flow).build();
     }
 
-    @PUT
-    @ProtectedApi(scopes = { ApiAccessConstants.ATTRIBUTES_WRITE_ACCESS })
-    public Response updateAttribute(@Valid GluuAttribute attribute) {
-        log.debug(" GluuAttribute details to update - attribute = " + attribute);
-        String inum = attribute.getInum();
-        checkResourceNotNull(inum, GLUU_ATTRIBUTE);
-        checkNotNull(attribute.getName(), AttributeNames.NAME);
-        checkNotNull(attribute.getDisplayName(), AttributeNames.DISPLAY_NAME);
-        checkResourceNotNull(attribute.getDataType(), AttributeNames.DATA_TYPE);
-        GluuAttribute existingAttribute = attributeService.getAttributeByInum(inum);
-        checkResourceNotNull(existingAttribute, GLUU_ATTRIBUTE);
-        attribute.setInum(existingAttribute.getInum());
-        attribute.setBaseDn(attributeService.getDnForAttribute(inum));
-        attributeService.updateAttribute(attribute);
-        GluuAttribute result = attributeService.getAttributeByInum(inum);
-        return Response.ok(result).build();
-    }
 
-    @PATCH
-    @Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
-    @ProtectedApi(scopes = { ApiAccessConstants.ATTRIBUTES_WRITE_ACCESS })
-    @Path(ApiConstants.INUM_PATH)
-    public Response patchAtribute(@PathParam(ApiConstants.INUM) @NotNull String inum, @NotNull String pathString)
-            throws JsonPatchException, IOException {
-        log.debug(" GluuAttribute details to patch - inum = " + inum + " , pathString = " + pathString);
-        GluuAttribute existingAttribute = attributeService.getAttributeByInum(inum);
-        checkResourceNotNull(existingAttribute, GLUU_ATTRIBUTE);
-
-        existingAttribute = Jackson.applyPatch(pathString, existingAttribute);
-        attributeService.updateAttribute(existingAttribute);
-        return Response.ok(existingAttribute).build();
-    }
 
     @DELETE
-    @Path(ApiConstants.INUM_PATH)
-    @ProtectedApi(scopes = { ApiAccessConstants.ATTRIBUTES_DELETE_ACCESS })
-    public Response deleteAttribute(@PathParam(ApiConstants.INUM) @NotNull String inum) {
-        log.debug(" GluuAttribute details to delete - inum = " + inum);
-        GluuAttribute attribute = attributeService.getAttributeByInum(inum);
-        checkResourceNotNull(attribute, GLUU_ATTRIBUTE);
-        attributeService.removeAttribute(attribute);
+    @Path(ApiConstants.QNAME_PATH)
+    @ProtectedApi(scopes = { ApiAccessConstants.AGAMA_DELETE_ACCESS })
+    public Response deleteAttribute(@PathParam(ApiConstants.QNAME) @NotNull String flowName) {
+        log.debug(" Flow to delete - flowName:{}", flowName);
+        Flow flow = agamaFlowService.getFlowByName(flowName);
+        checkResourceNotNull(flow, AGAMA_FLOW);
+        agamaFlowService.removeAgamaFlow(flow);
         return Response.noContent().build();
     }
 
+    
+    
 }
--- jans-config-api/server/src/main/java/io/jans/configapi/service/auth/AgamaFlowService.java
+++ jans-config-api/server/src/main/java/io/jans/configapi/service/auth/AgamaFlowService.java
@@ -1,50 +1,118 @@
 
 package io.jans.configapi.service.auth;
 
+import io.jans.agama.model.Flow;
+import static io.jans.as.model.util.Util.escapeLog;
+import io.jans.orm.PersistenceEntryManager;
+import io.jans.orm.search.filter.Filter;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.slf4j.Logger;
 
-import io.jans.orm.PersistenceEntryManager;
-import io.jans.util.StringHelper;
-import io.jans.agama.model.Flow;
-
 @ApplicationScoped
-public class AgamaFlowService  implements Serializable {
+public class AgamaFlowService implements Serializable {
 
     private static final long serialVersionUID = 7912416439116338984L;
-    
+
+    private static final String AGAMA_BASE = "ou=agama,o=jans";
+    public static final String AGAMA_FLOWS_BASE = "ou=flows," + AGAMA_BASE;
+
     @Inject
     private transient Logger logger;
 
     @Inject
     private transient PersistenceEntryManager persistenceEntryManager;
 
+    public Flow getClientByInum(String inum) {
+        Flow result = null;
+        try {
+            result = persistenceEntryManager.find(Flow.class, getFlowByDn(inum));
+        } catch (Exception ex) {
+            logger.error("Failed to load client entry", ex);
+        }
+        return result;
+    }
+
+    public List<Flow> searchAgamaFlows(String pattern, int sizeLimit) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Search Agama Flow with pattern:{}, sizeLimit:{} ", escapeLog(pattern), escapeLog(sizeLimit));
+        }
+
+        String[] targetArray = new String[] { pattern };
+        Filter searchFilter = Filter.createORFilter(
+                Filter.createSubstringFilter(Flow.ATTR_NAMES.QNAME, null, targetArray, null),
+                Filter.createSubstringFilter(Flow.ATTR_NAMES.META, null, targetArray, null));
+
+        logger.debug("Agama Flows with matching searchFilter:{}", searchFilter);
+        return persistenceEntryManager.findEntries(getAgamaFlowDn(), Flow.class, searchFilter, sizeLimit);
+    }
+
+    public List<Flow> searchAgamaFlows(String pattern, int sizeLimit, boolean enabled) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Search Agama Flow with pattern:{}, sizeLimit:{}, enabled:{} ", escapeLog(pattern),
+                    escapeLog(sizeLimit), escapeLog(enabled));
+        }
+
+        String[] targetArray = new String[] { pattern };
+        Filter searchFilter = Filter.createORFilter(
+                Filter.createSubstringFilter(Flow.ATTR_NAMES.QNAME, null, targetArray, null),
+                Filter.createSubstringFilter(Flow.ATTR_NAMES.META, null, targetArray, null),
+                Filter.createEqualityFilter("jansEnabled", enabled));
+
+        logger.debug("Agama Flows with searchFilter:{}", searchFilter);
+        return persistenceEntryManager.findEntries(getAgamaFlowDn(), Flow.class, searchFilter, sizeLimit);
+
+    }
+
+    public List<Flow> getAllAgamaFlows(int sizeLimit) {
+        return persistenceEntryManager.findEntries(getAgamaFlowDn(), Flow.class, null, sizeLimit);
+    }
+
+    public List<Flow> getAllFlows() {
+        return persistenceEntryManager.findEntries(getAgamaFlowDn(), Flow.class, null);
+    }
+
+    public Flow getFlowByName(String flowName) {
+        List<Flow> flows = persistenceEntryManager.findEntries(getAgamaFlowDn(), Flow.class,
+                Filter.createEqualityFilter(Flow.ATTR_NAMES.QNAME, flowName), 1);
+        logger.debug("Agama Flow with flowName:{} flows:{}", flowName, flows);
+        if (!flows.isEmpty()) {
+            return flows.get(0);
+        }
+        return null;
+    }
+
+    public Flow getFlowByDn(String dn) {
+        try {
+            return persistenceEntryManager.find(Flow.class, dn);
+        } catch (Exception e) {
+            logger.warn("", e);
+            return null;
+        }
+    }
+
     public void addAgamaFlow(Flow flow) {
         logger.error("Added Agama Flow:{}", flow);
         persistenceEntryManager.persist(flow);
     }
-    
+
     public void updateClient(Flow flow) {
         logger.error("Update Agama Flow:{}", flow);
         persistenceEntryManager.merge(flow);
     }
-    
+
     public void removeAgamaFlow(Flow flow) {
         logger.error("Remove Agama Flow:{}", flow);
         persistenceEntryManager.removeRecursively(flow.getDn(), Flow.class);
     }
-    
-    public String getDnForClient(String inum) {
-        String orgDn = organizationService.getDnForOrganization();
-        if (StringHelper.isEmpty(inum)) {
-            return String.format("ou=clients,%s", orgDn);
-        }
-        return String.format("inum=%s,ou=clients,%s", inum, orgDn);
-    }
 
+    public String getAgamaFlowDn() {
+        return AGAMA_FLOWS_BASE;
+    }
 
 }
